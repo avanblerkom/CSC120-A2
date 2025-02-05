9854164949a7dcf49eab10922125811319a5144d
Alex Van Blerkom <avanblerkom@smith.edu>
2025-02-05 16:59:33 -0500

    attributes added

--- computer.py
+++ computer.py
@@ -1,10 +1,22 @@
 class Computer:
 
     # What attributes will it need?
-
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, desc:str, proc:str, hd:int, mem:int, os:str, year:int, price:int):
+        self.description = desc
+        self.processor_type = proc
+        self.hard_drive_capacity = hd
+        self.memory = mem
+        self.operating_system = os
+        self.year_made = year
 
     # What methods will you need?
+    
