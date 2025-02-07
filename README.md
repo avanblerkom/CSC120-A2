2233e544c75acde9a76e3e43c34d47955b7570ac
Alex Van Blerkom <avanblerkom@smith.edu>
2025-02-07 17:17:20 -0500

    before adding error message in computer

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 This repository contains starter code for A2: Object-ification, developed for CSC120: Object-Oriented Programming as taught by Prof. R. Jordan Crouser and Prof. Johanna Brewer at Smith College in Spring 2024.
   
  `procedural_resale_shop.py` contains a procedural program to perform the basic functions of a computer resale store:
-   - buying a computer (add to inventory)
-   - selling a computer (remove from inventory)
-   - updating the price of an item in the inventory
-   - printing the inventory
-   - refurbishing a computer (update price based on age of machine, optionally update OS)
+   - * buying a computer (add to inventory)
+   - * selling a computer (remove from inventory)
+   - * updating the price of an item in the inventory
+   - * printing the inventory
+   - * refurbishing a computer (update price based on age of machine, optionally update OS)
    
   `main.py` contains a simple demonstration of each of these functions, plus a helper function to create a "computer"
   
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -7,10 +7,49 @@ class ResaleShop:
     
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, inventory):
+        #1. set up an attribute to store the inventory
+        self.inventory = inventory
+
 
     # What methods will you need?
-    def buy(self):
+    def buy(self, computer: Computer):
         #1. call Computer(...) constructor to create a new Computer instance 
-        #2. call inventory.append(...) to add the new omputer instance to the inventory
+        self.inventory.append(computer)
+        #2. call inventory.append(...) to add the new computer instance to the inventory
+        return self.inventory.index(computer)
+    
+    def sell(self, computer: Computer):
+        if computer in self.inventory:
+            self.inventory.remove(computer)
+            print("Item sold!")
+    
+    def print_inventory(self):
+        # If the inventory is not empty
+        if self.inventory:
+            # For each item
+            for item in self.inventory:
+                # Print its details
+                print(f'Item ID: {self.inventory.index(item)} : {item}')
+        else:
+            print("No inventory to display.")
+
+def main():
+    computer1: Computer = Computer("1998 MacBook Pro", "Intel", 256, 16, "High Sierra", 2011, 1000)
+    computer2: Computer = Computer("2024 MacBook Pro", "Intel", 300, 20, "High Sierra", 2024, 1500)
+    resaleshop: ResaleShop = ResaleShop([])
+    resaleshop.buy(computer1)
+    resaleshop.buy(computer2)
+    resaleshop.print_inventory()
+    resaleshop.sell(computer2)
+    resaleshop.print_inventory()
+    print(computer1.price)
+    Computer.refurbish(computer1, "Monterey")
+    resaleshop.print_inventory()
+    print(computer1.operating_system)
+    print(computer1.price)
+
+
+if __name__ == "__main__": 
+    main()
+    
